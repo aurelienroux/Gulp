a1eae2932e023c56294128154e56786b39d28e75
Aurélien Roux <aureroux13@me.com>
2017-09-30 22:29:17 -0400

    add js tasks concat and uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,26 @@
+//Dependencies required
 var gulp = require('gulp');
+//IMAGES
 var imagemin = require('gulp-imagemin');
 var changed = require('gulp-changed');
+//CSS
+var concat = require('gulp-concat');
 var autoprefix = require('gulp-autoprefixer');
 var minifyCSS = require('gulp-minify-css');
-var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+//LIVERELOAD
 var browserSync = require('browser-sync').create();
 
+
+//javascript optimization
+gulp.task('scripts', function(){
+  gulp.src(['src/scripts/*.js'])
+  .pipe(concat('app.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('build/scripts/'));
+})
+
+//image minification -- only changes if necessary
 gulp.task('imagemin', function(){
   var img_src = 'src/images/**/*';
   var img_dest = 'build/images';
@@ -15,17 +30,20 @@ gulp.task('imagemin', function(){
   .pipe(gulp.dest(img_dest));
 });
 
+// concat CSS styles, autoprefix and minification
 gulp.task('styles', function() {
   gulp.src(['src/styles/*.css'])
   .pipe(concat('styles.css'))
   .pipe(autoprefix('last 2 versions'))
   .pipe(minifyCSS())
   .pipe(gulp.dest('build/styles'))
+  //browserSync reload application
   .pipe(browserSync.reload({
     stream: true
   }))
 })
 
+//broswerSync reload for CSS changes
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
@@ -34,6 +52,7 @@ gulp.task('browserSync', function() {
   })
 });
 
-gulp.task('default', ['imagemin', 'browserSync', 'styles'], function(){
+// Gulp defaut tasks
+gulp.task('default', ['imagemin', 'browserSync', 'styles', 'scripts'], function(){
   gulp.watch('src/styles/*.css', ['styles']);
 });
