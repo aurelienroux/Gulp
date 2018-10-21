e576fd10a2cb58da0d6c02bb4c08e6215da23a77
Aurélien Roux <aureroux13@gmail.com>
2018-10-21 11:31:59 -0400

    create a sass-lint config file

--- Src/gulp-config/gulp-sass-lint.js
+++ Src/gulp-config/gulp-sass-lint.js
@@ -3,8 +3,10 @@ const sasslint = require('gulp-sass-lint');
 const config = require('./gulp-config');
 
 gulp.task('sasslint', function() {
-  return gulp.src(config.styles.src)
-    .pipe(sasslint())
+  return gulp.src(config.styles.files)
+    .pipe(sasslint({
+      configfile: '../../.sass-lint.yml'
+    }))
     .pipe(sasslint.format())
     .pipe(sasslint.failOnError());
 })
